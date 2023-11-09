a10158aa18162dd73198733decb84c5bb1853046
Corey Schroeder <codingcorey365@gmail.com>
2023-11-09 14:27:52 -0600

    Static Exercise Final commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine($"It is {TempConverter.FahrenheitToCelsius(80)} degrees Celsius.");
+            Console.WriteLine($"It is {TempConverter.CelsiusToFahrenheit(80)} degrees Fahrenheit.");
+
+            
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double f) 
+        {
+            var c = ((f - 32) * 5.0) / 9;
+
+            return c;
+        }
+
+        public static double CelsiusToFahrenheit(double c)
+        {
+            var f = c * (9.0/5) + 32;
+
+            return f;
+        }
+    }
+}
